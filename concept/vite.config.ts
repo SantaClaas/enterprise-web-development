08ef8a8870113167daecfd4ca81ec48e79ccadd1
Claas <claas@claas.dev>
2026-05-02 16:46:11 +0200

    Delete unused concept project

--- concept/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
-
-export default defineConfig({
-  plugins: [tailwindcss(), viteSingleFile()],
-});
