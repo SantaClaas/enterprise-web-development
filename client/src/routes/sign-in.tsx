07bd2f65676211985454975f0cd0dba628a58633
Claas <claas@claas.dev>
2026-06-03 17:04:15 +0000

    Go back because empty input detection does not work in CSS

--- client/src/routes/sign-in.tsx
+++ client/src/routes/sign-in.tsx
@@ -1,20 +1,20 @@
 import { createFileRoute } from "@tanstack/solid-router";
 
 import Body from "../Body";
-import Icon from "../Icon";
-import { TextFieldInput, TextFieldLabel, TextFieldLabelText } from "../TextField";
 
 export const Route = createFileRoute("/sign-in")({
   component: SignIn,
 });
 
 function SignIn() {
-  async function handleSignIn() {
+  async function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
     console.debug("Signing in...");
 
-    const user = "yealch";
-    const password = "password";
-    const value = btoa(`${user}:${password}`);
+    const form = event.currentTarget as HTMLFormElement;
+    const username = form.namedItem("username")?.value;
+    const password = form.namedItem("password")?.value;
+    const value = btoa(`${username}:${password}`);
     const response = await fetch("/api/login", {
       method: "POST",
       headers: {
@@ -35,22 +35,37 @@ function SignIn() {
     <Body class="h-dvh p-4 md:p-6">
       <main class="grid h-full items-end">
         <h1 class="text-center font-serif text-6xl font-black text-purple-900">Welcome</h1>
-        <section class="rounded-3xl p-6 text-base leading-6">
-          <form>
-            <label class="text-field">
-              <span>Username</span>
-              <input type="text" name="username" autocomplete="username" required />
-            </label>
-            <label class="text-field mt-4">
-              <span>Password</span>
-              <input type="password" name="password" autocomplete="current-password" required />
-            </label>
-            <button type="submit" class="button mt-6" data-variant="primary">
-              Sign in
-            </button>
-          </form>
-          {/* TODO: Button states styles */}
-          {/* <button
+        <form onSubmit={handleSubmit} class="rounded-3xl p-6 text-base leading-6">
+          <label for="username" class="text-label-lg block">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            autocomplete="username"
+            required
+            class="text-field mt-1 w-full"
+          />
+
+          <label for="password" class="text-label-lg text-on-surface-variant mt-4 block">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autocomplete="current-password"
+            required
+            class="text-field mt-1 w-full"
+          />
+
+          <button type="submit" class="button mt-6" data-variant="primary">
+            Sign in
+          </button>
+        </form>
+        {/* TODO: Button states styles */}
+        {/* <button
             onClick={handleSignIn}
             class="bg-primary text-on-primary mt-6 flex w-full items-center justify-center gap-2 rounded-full px-6 py-4"
           >
@@ -58,7 +73,6 @@ function SignIn() {
             <span>Sign in with Passkey</span>
           </button>
           <button class="mt-4 block w-full px-6 py-4 text-purple-700">Register</button> */}
-        </section>
       </main>
     </Body>
   );
