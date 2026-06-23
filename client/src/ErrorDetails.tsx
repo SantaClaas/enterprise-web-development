fc484c07090dc3838c7368aed90d3aba7f735065
Claas <claas@claas.dev>
2026-06-23 20:19:45 +0000

    Fix overflow

--- client/src/ErrorDetails.tsx
+++ client/src/ErrorDetails.tsx
@@ -6,7 +6,7 @@ export function ErrorDetails(
 ) {
   return (
     <>
-      <article class="bg-error-container text-on-error-container grid grid-cols-[1fr_auto] rounded p-6">
+      <article class="bg-error-container text-on-error-container mt-6 grid grid-cols-[1fr_auto] rounded p-6">
         <h2 class="text-title-lg col-span-full">{properties.title}</h2>
         <p class="text-body-lg col-span-full mt-2">{properties.explainer}</p>
 
@@ -15,10 +15,10 @@ export function ErrorDetails(
             <summary>Technical details</summary>
 
             <Show when={properties.error}>
-              <pre>{properties.error.stack}</pre>
+              <pre class="overflow-x-scroll">{properties.error.stack}</pre>
             </Show>
             <Show when={properties.info}>
-              <pre>{JSON.stringify(properties.info, null, 2)}</pre>
+              <pre class="overflow-x-scroll">{JSON.stringify(properties.info, null, 2)}</pre>
             </Show>
           </details>
         </Show>
